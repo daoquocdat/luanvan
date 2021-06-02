26313dc1ec65a50db612ed87f65e17d03523cee1
daoquocdat <daoquocdat1807@gmail.com>
2021-06-02 23:41:24 +0700

    test

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const morgan = require('morgan')
+const handlebars  = require('express-handlebars')
+const path = require('path')
+const app = express()
+const port = 4000
+
+app.use(morgan(`combined`))
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views' ,path.join(__dirname, 'resource/views'))
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
